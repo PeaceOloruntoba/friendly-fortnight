c67d296accb7537ea41749ffc46ebafc44ff1375
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 19:05:56 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        screen: "100dvh",
+        "screen-small": "100svh",
+        "screen-large": "100lvh",
+      },
+      width: {
+        screen: "100dvw",
+        "screen-small": "100svw",
+        "screen-large": "100lvw",
+      },
+    },
   },
   plugins: [],
 }
-
