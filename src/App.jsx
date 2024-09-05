33787989b9598f104f8696bc9e400411f8bb52e0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 19:20:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
@@ -7,7 +8,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route element={<RootLayout />}>
-            <Route path="" />
+            <Route path="" element={<Home />} />
           </Route>
         </Routes>
       </Router>
