0d6a148c9fda0248af9f202928cfad23b0d4d92d
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 19:19:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
 
 export default function App() {
-  return <>
-  <Router>
-    <Routes>
-      
-    </Routes>
-  </Router>
-  </>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route path="" />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
