7fea959490fe4fcb764f52ac450866d488b61c46
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-05 19:18:46 +0100

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/containers/Header";
+import Navbar from "../components/containers/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/containers/Footer";
 
 export default function RootLayout() {
   return (
-    <div>
-      
+    <div className="w-screen h-screen flex flex-col">
+      <Header />
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
 }
