cfafb2ef92aa8203bd0c674ef1af268eaa420c14
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-08 22:04:31 +0100

    nnmnm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Hero from "../components/ui/Home/Hero";
 import About from "../components/ui/Home/About";
+import Services from "../components/ui/Home/Services";
 
 export default function Home() {
   return (
-    <div className="bg-black text-white">
+    <div className="text-white">
       <Hero />
       <About />
-      Home
+      <Services />
     </div>
   );
 }
