e1cd3b3e3d899b8dc59a10dd5e1a068593aa3809
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-09 15:17:28 +0100

    Update ServiceCard.jsx

--- src/components/containers/ServiceCard.jsx
+++ src/components/containers/ServiceCard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-export default function ServiceCard() {
+export default function ServiceCard({ title, content, image, link }) {
   return (
-    <div>
-      
+    <div className="bg-white p-6 rounded-xl shadow-xl shadow-gray-200 group transform transition-transform duration-500 hover:-translate-y-4 grid items-start justify-start">
+      <img
+        src={image}
+        alt={title}
+        className="transform w-24 group-hover:scale-x-[-1] transition-transform duration-500 rounded-full"
+      />
+      <p className="text-2xl font-semibold mt-4">{title}</p>
+      <p className="text-gray-700 mt-2">{content}</p>
+      <Link
+        to={link}
+        className="flex items-center justify-start gap-2 hover:gap-1 duration-500 text-purple-800 font-semibold"
+      >
+        <FaArrowRight />
+        Learn More
+      </Link>
     </div>
-  )
+  );
 }
