0b863940a66b383ff5d5b66bd34d5ae10180fcaa
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-09 15:08:24 +0100

    Create services.js

---
+++ src/data/services.js
@@ -0,0 +1,74 @@
+const cardData = [
+  {
+    id: "it-solution",
+    title: "Easing Life IT Solution",
+    content:
+      "We offer diverse IT services, including web and mobile app development, system implementation, cybersecurity training, CCTV installation, and IT consultation. Our tailored solutions leverage cutting-edge technology and expertise, ensuring seamless operations, enhanced security, and optimized business processes.",
+    image: "/assets/images/logo.png",
+    link: "/it-solution",
+  },
+  {
+    id: "laundry",
+    title: "Easing Life Laundry Services",
+    content:
+      "Discover the ease and reliability of our professional laundry service. We recognize the value of clean, well-maintained clothes, and our skilled team guarantees that your items are thoroughly cleaned, pressed, and delivered to you with the utmost care and efficiency.",
+    image: "/assets/images/logo.png",
+    link: "/laundry",
+  },
+  {
+    id: "cafe",
+    title: "Easing Life Cafe and Printing Services",
+    content:
+      "Visit our vibrant café and have a nice cup of coffee while taking advantage of our complete printing services. We serve a wide range of printing demands, assuring accuracy, promptness, and high-quality outputs. From helping with online registrations to school results checking and more",
+    image: "/assets/images/logo.png",
+    link: "/cafe",
+  },
+  {
+    id: "forex",
+    title: "Forex Trading & Cryptocurrency Investment",
+    content:
+      "With our comprehensive forex trading and cryptocurrency investment services, you can stay ahead in the volatile world of finance. Our professional staff delivers essential training, insights, and direction to assist you in efficiently navigating these markets, allowing you to make educated investment decisions.",
+    image: "/assets/images/logo.png",
+    link: "/forex",
+  },
+  {
+    id: "educational",
+    title: "Easing Life Educational and Travel Consult",
+    content:
+      "We are dedicated to aiding individuals inrealizing their dreams of studying and working abroad. We provide complete help, from advising you through the selection of study programs and tertiary education opportunities to assisting you with filling out school applications. In addition, we offer travel arrangements and documentation services to make your journey easier.",
+    image: "/assets/images/logo.png",
+    link: "/educational",
+  },
+  {
+    id: "streaming",
+    title: "Easing Life Online Streaming Service",
+    content:
+      "Use our cutting-edge online streaming service to connect with your audience and share your content with them. Our dependable platform provideshigh-quality streaming experiences, enabling successful communication, participation, and interaction, whether for educational purposes, live events, or virtual meetings.",
+    image: "/assets/images/logo.png",
+    link: "/streaming",
+  },
+  {
+    id: "docease",
+    title: "Easing Life DocEase Solutions",
+    content:
+      "Use Easinglife DocEase Solutions to streamline your document application procedures. We provide quick and easy access to important documents such as birth certificates, passports, and company certificates. Our streamlined processes save you time and effort, resulting in an easy and trouble-free experience.",
+    image: "/assets/images/logo.png",
+    link: "/docease",
+  },
+  {
+    id: "financial",
+    title: "Easing Life Financial Solutions",
+    content:
+      "Our financial solutions section provides a wide variety of services, including accounting,y financial statement creation and analysis, tax planning, payroll administration, budgeting, investment and portfolio management, and financial counselling. We provide people and businesses with competent financial advice and support, providing proper financial management and decision-making.",
+    image: "/assets/images/logo.png",
+    link: "/financial",
+  },
+  {
+    id: "easyride",
+    title: "Easing Life Easy Ride",
+    content:
+      "Our IT section provides a variety of services, including website development, mobile app development, management system implementation, cyber security training, CCTV installation, and general IT consultation. We build customized solutions to match your particular demands using cutting-edge technology and experience, assuring flawless operations, increased security, and optimized business processes.",
+    image: "/assets/images/logo.png",
+    link: "/easyride",
+  },
+];
