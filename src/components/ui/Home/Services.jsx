d9af9a03fa8d727008dc934651ebd05f74262709
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-09 15:51:00 +0100

    Update Services.jsx

--- src/components/ui/Home/Services.jsx
+++ src/components/ui/Home/Services.jsx
@@ -11,14 +11,14 @@ export default function Services() {
         <div className="bg-blue-600 px-8 md:px-16 h-[1px]"></div>
       </div>
       <span className="text-xl md:text-3xl font-bold">Our Services</span>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 py-12 px-44">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 py-12 md:px-24 px-4">
         {servicesData?.map((service) => (
           <ServiceCard
             key={service?.id}
             title={service?.title}
             content={service?.content}
-            image={card?.image}
-            link={card?.link}
+            image={service?.image}
+            link={service?.link}
           />
         ))}
       </div>
