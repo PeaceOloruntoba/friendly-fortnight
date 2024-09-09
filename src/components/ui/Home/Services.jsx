27a82b555ced45b2dbc9f3b15ee0c93457c6ee29
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-09 15:43:15 +0100

    Update Services.jsx

--- src/components/ui/Home/Services.jsx
+++ src/components/ui/Home/Services.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Services() {
   return (
-    <div className="w-full bg-white flex flex-col items-center text-black/70">
+    <div className="w-full bg-white flex flex-col items-center text-black/70 py-8">
       <div className="flex gap-4 items-center justify-center">
         <div className="bg-blue-600 px-8 md:px-16 h-[1px]"></div>
         <span className="font-semibold text-sm md:text-xl">What we offer</span>
-        <div className="bg-blue-600 px-16 h-[1px]"></div>
+        <div className="bg-blue-600 px-8 md:px-16 h-[1px]"></div>
       </div>
       <span className="text-xl md:text-3xl font-bold">Our Services</span>
     </div>
