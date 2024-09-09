170452897fc2a3003d02d27a9920d28d08e557ac
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-09 16:35:56 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,7 @@
-import logo from "./images/logo.png"
-import aboutimg from "./images/about.png"
-import jumbotron from "./images/jumbotron.png"
-import homebottom from "./images/homebottom.png"
+import logo from "./images/logo.png";
+import aboutimg from "./images/about.png";
+import jumbotron from "./images/jumbotron.png";
+import homebottom from "./images/homebottom.png";
+import choose from "./images/choose-1.png";
 
-export default {logo, aboutimg, jumbotron,homebottom }
+export default { logo, aboutimg, jumbotron, homebottom, choose };
