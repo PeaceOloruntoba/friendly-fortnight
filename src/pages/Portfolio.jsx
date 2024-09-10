09e22513ede9933c76a53dbe276bba5925ea81d7
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:30:20 +0100

    Update Portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import PageHead from "../components/shared/PageHead";
 
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div className="flex flex-col w-full">
+      <PageHead name={"Portfolio"} />
+    </div>
+  );
 }
