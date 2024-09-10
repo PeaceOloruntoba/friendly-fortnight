fda0c5e68f75f7aed299b617a2a475d782654a7c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 12:15:34 +0100

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PageHead from "../components/shared/PageHead";
+import ContactForm from "../components/ui/contact/ContactForm";
 
 export default function Contact() {
   return (
     <div className="flex flex-col w-full">
       <PageHead name={"Contact"} />
+      <ContactForm />
     </div>
   );
 }
