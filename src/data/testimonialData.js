b2c6d9868bb560f246959b10185879b2e0fd4ff3
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 12:18:25 +0100

    Create testimonialData.js

---
+++ src/data/testimonialData.js
@@ -0,0 +1,32 @@
+import images from "../assets";
+
+const me = images?.mee;
+
+export const testimonialData = [
+  {
+    quote: "Flowbite is a true gem in the world of web development...",
+    author: "Micheal Gough",
+    company: "CEO at Google",
+    avatar: me,
+  },
+  {
+    quote:
+      "TypeScript has become an indispensable part of our development stack...",
+    author: "Micheal Gough",
+    company: "CEO at Google",
+    avatar: me,
+  },
+  {
+    quote: "Next.js has revolutionized the way we build web applications...",
+    author: "Micheal Gough",
+    company: "CEO at Google",
+    avatar: me,
+  },
+  {
+    quote:
+      "Tailwind CSS has been a game-changer for our frontend development workflow...",
+    author: "Micheal Jordan",
+    company: "CEO at Microsoft",
+    avatar: me,
+  },
+];
