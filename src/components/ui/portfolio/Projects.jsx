2cf05bf3da3451d51be239929cd630a220df0f4c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:33:43 +0100

    ,,,

--- src/components/ui/portfolio/Projects.jsx
+++ src/components/ui/portfolio/Projects.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PortfolioCard from "../../containers/PortfolioCard";
+import { folioData } from "../../../data/portfolioData";
 
 export default function Projects() {
   return (
     <div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 py-8 px-40 pb-32">
-        {folioData.map((card, index) => (
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 py-8 px-4 md:px-12">
+        {folioData?.map((card, index) => (
           <PortfolioCard
             key={index}
             title={card.title}
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PageHead from "../components/shared/PageHead";
+import Projects from "../components/ui/portfolio/Projects";
 
 export default function Portfolio() {
   return (
     <div className="flex flex-col w-full">
       <PageHead name={"Portfolio"} />
+      <Projects />
     </div>
   );
 }
