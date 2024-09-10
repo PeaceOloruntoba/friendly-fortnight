c21ab3080909c5d63dbd22aa61e093e7a7ff6f39
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:03:48 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PageHead from "../components/shared/PageHead";
 import Hero from "../components/ui/About/Hero";
+import More from "../components/ui/About/More";
 
 export default function About() {
   return (
     <div className="flex flex-col w-full">
       <PageHead name={"About Us"} />
       <Hero />
+      <More />
     </div>
   );
 }
