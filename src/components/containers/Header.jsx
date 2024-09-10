092ce8a72d17bb1e0b1789ebb22411a3c41c6204
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:40:20 +0100

    Update Header.jsx

--- src/components/containers/Header.jsx
+++ src/components/containers/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      Header
+    <div className="w-full flex items-center justify-end md:justify-between ">
+      <span className="md:block hidden">email</span>
+      <span>social icons</span>
     </div>
-  )
+  );
 }
