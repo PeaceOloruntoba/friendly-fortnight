089509af0554ccc092408312c2c14b27a17914b5
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:08:43 +0100

    Update WorkProcess.jsx

--- src/components/ui/About/WorkProcess.jsx
+++ src/components/ui/About/WorkProcess.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function WorkProcess() {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center bg-[#f7f8f8]">
+      <p className="text-[#ff9800] text-lg md:text-2xl font-semibold">
+        4 Step Work Process
+      </p>
+      <p className="md:text-2xl text-sm font-semibold">Working Process</p>
+      <div className="grid md:grid-cols-4">
+        <div className="flex flex-col">
+          <span>Research</span>
+          <span>Do Research</span>
+          <span>01</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
