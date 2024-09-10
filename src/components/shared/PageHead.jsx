3cbd2be796a2efa9efe0449abc8df84d81fb65f0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 06:48:20 +0100

    Update PageHead.jsx

--- src/components/shared/PageHead.jsx
+++ src/components/shared/PageHead.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { FaDotCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-export default function PageHead() {
+export default function PageHead({ name }) {
   return (
-    <div>
-      
+    <div className="grid banner py-40 text-white text-center">
+      <p className="font-bold text-5xl p-6">{name}</p>
+      <p className="flex gap-4 items-center justify-center text-xl">
+        <Link to="/" className="hover:text-blue-600 transition duration-300">
+          Home
+        </Link>
+        <FaDotCircle /> {name}
+      </p>
     </div>
-  )
+  );
 }
