747eb1e67f1cf84200eebc06f61ed161dad8e917
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:04:53 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PageHead from "../components/shared/PageHead";
 import Hero from "../components/ui/About/Hero";
 import More from "../components/ui/About/More";
+import Team from "../components/ui/Home/Team";
 
 export default function About() {
   return (
@@ -9,6 +10,7 @@ export default function About() {
       <PageHead name={"About Us"} />
       <Hero />
       <More />
+      <Team />
     </div>
   );
 }
