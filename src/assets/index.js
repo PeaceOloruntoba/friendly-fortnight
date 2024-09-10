013d0421967e65a620a8bcc43f72b4d73b362aee
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 06:32:15 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import aboutimg from "./images/about.png";
 import jumbotron from "./images/jumbotron.png";
 import homebottom from "./images/homebottom.png";
 import choose from "./images/choose-1.png";
+import mee from "./images/mee.jpg";
 
-export default { logo, aboutimg, jumbotron, homebottom, choose };
+export default { logo, aboutimg, jumbotron, homebottom, choose, mee };
