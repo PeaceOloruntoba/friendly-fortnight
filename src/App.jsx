ca8737bf1feb65a777e39effa9a2536a5d2a3c3f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:44:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,35 @@ import About from "./pages/About";
 import Services from "./pages/Services";
 import Portfolio from "./pages/Portfolio";
 import ServicesDetails from "./pages/ServicesDetails";
+import { Suspense } from "react";
 
 export default function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route element={<RootLayout />}>
-            <Route path="" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="services" element={<Services />} />
-            <Route path="services/:serviceName" element={<ServicesDetails />} />
-            <Route path="portfolio" element={<Portfolio />} />
-          </Route>
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="w-screen h-screen flex items-center justify-center text-center">
+              <span className="text-xl font-semibold animate-pulse">
+                Loading
+              </span>
+            </div>
+          }
+        >
+          <ScrollToTop />
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route path="" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="services" element={<Services />} />
+              <Route
+                path="services/:serviceName"
+                element={<ServicesDetails />}
+              />
+              <Route path="portfolio" element={<Portfolio />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </Router>
     </>
   );
