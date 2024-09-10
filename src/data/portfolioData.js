8b9021cde34954eaf26268a0af9669717bd1f142
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:23:58 +0100

    Create portfolioData.js

---
+++ src/data/portfolioData.js
@@ -0,0 +1,42 @@
+import images from "../assets"
+
+
+
+export const folioData = [
+  {
+    title: "App Store | Social Media",
+    content: "Creative Web Design",
+    image: "/assets/images/portfolio-1.jpg",
+    link: "/",
+  },
+  {
+    title: "App Store | Social Media",
+    content: "Creative Web Design",
+    image: "/assets/images/portfolio-2.jpg",
+    link: "/",
+  },
+  {
+    title: "App Store | Social Media",
+    content: "Creative Web Design",
+    image: "/assets/images/portfolio-3.jpg",
+    link: "/",
+  },
+  {
+    title: "App Store | Social Media",
+    content: "Creative Web Design",
+    image: "/assets/images/portfolio-4.jpg",
+    link: "/",
+  },
+  {
+    title: "App Store | Social Media",
+    content: "Creative Web Design",
+    image: "/assets/images/portfolio-5.jpg",
+    link: "/",
+  },
+  {
+    title: "App Store | Social Media",
+    content: "Creative Web Design",
+    image: "/assets/images/portfolio-6.jpg",
+    link: "/",
+  },
+];
