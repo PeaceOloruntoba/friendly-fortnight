7b9ea1bf8e28c77ebaf0d04439b5c1d7cbecac27
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:30:59 +0100

    Update Projects.jsx

--- src/components/ui/portfolio/Projects.jsx
+++ src/components/ui/portfolio/Projects.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Projects() {
   return (
     <div>
-      
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 py-8 px-40 pb-32">
+        {folioData.map((card, index) => (
+          <PortfolioCard
+            key={index}
+            title={card.title}
+            content={card.content}
+            image={card.image}
+            link={card.link}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
