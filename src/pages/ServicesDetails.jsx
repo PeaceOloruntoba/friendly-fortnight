a388590e6c80baaaf4467e12ae8d67b73ed943af
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:35:10 +0100

    Update ServicesDetails.jsx

--- src/pages/ServicesDetails.jsx
+++ src/pages/ServicesDetails.jsx
@@ -3,5 +3,9 @@ import { useParams } from "react-router-dom";
 
 export default function ServicesDetails() {
   const { serviceName } = useParams();
-  return <div>{serviceName}</div>;
+  return (
+    <div className="flex flex-col w-full items-center">
+      <span>{serviceName}</span>
+    </div>
+  );
 }
