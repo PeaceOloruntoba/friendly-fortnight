f8d9c099dd89c8f62abfe1a820b3305b11a572a9
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 12:21:01 +0100

    Update Testimonial.jsx

--- src/components/ui/Home/Testimonial.jsx
+++ src/components/ui/Home/Testimonial.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import TestimonialSlider from "./TestimonialSlider";
+import { testimonialData } from "../../../data/testimonialData";
 
 export default function Testimonial() {
   return (
-    <div>
-      
+    <div className="p-10 bg-white" id="testimonial">
+      <p className="text-3xl font-bold text-center mb-4">Testimonials</p>
+      <TestimonialSlider testimonials={testimonialData} />
     </div>
-  )
+  );
 }
