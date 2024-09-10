7f55f8508677cb942eb64643f92348410e9549d1
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 12:28:16 +0100

    Update Testimonial.jsx

--- src/components/ui/Home/Testimonial.jsx
+++ src/components/ui/Home/Testimonial.jsx
@@ -4,8 +4,8 @@ import { testimonialData } from "../../../data/testimonialData";
 
 export default function Testimonial() {
   return (
-    <div className="p-10 bg-white" id="testimonial">
-      <p className="text-3xl font-bold text-center mb-4">Testimonials</p>
+    <div className="p-3 md:p-8 bg-white" id="testimonial">
+      <p className="md:text-2xl text-lg font-bold text-center mb-4">Testimonials</p>
       <TestimonialSlider testimonials={testimonialData} />
     </div>
   );
