d333c4c051a5c8e7190e21deef1e1df6d13d3993
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:20:38 +0100

    Update Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import PageHead from "../components/shared/PageHead";
 import Hero from "../components/ui/Services/Hero";
+import WhyWhy from "../components/ui/Home/WhyWhy";
+import Contact from "../components/ui/Home/Contact";
 
 export default function Services() {
   return (
     <div className="flex flex-col w-full">
       <PageHead name={"Services"} />
       <Hero />
+      <WhyWhy />
+      <Contact />
     </div>
   );
 }
