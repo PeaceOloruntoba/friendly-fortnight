5df93981f907e783aff1d03e4bb7698265c77865
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 07:27:56 +0100

    Update portfolioData.js

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,42 +1,47 @@
-import images from "../assets"
+import images from "../assets";
 
-
+const portfolio1 = images?.portfolio1;
+const portfolio2 = images?.portfolio2;
+const portfolio3 = images?.portfolio3;
+const portfolio4 = images?.portfolio4;
+const portfolio5 = images?.portfolio5;
+const portfolio6 = images?.portfolio6;
 
 export const folioData = [
   {
     title: "App Store | Social Media",
     content: "Creative Web Design",
-    image: "/assets/images/portfolio-1.jpg",
+    image: portfolio1,
     link: "/",
   },
   {
     title: "App Store | Social Media",
     content: "Creative Web Design",
-    image: "/assets/images/portfolio-2.jpg",
+    image: portfolio2,
     link: "/",
   },
   {
     title: "App Store | Social Media",
     content: "Creative Web Design",
-    image: "/assets/images/portfolio-3.jpg",
+    image: portfolio3,
     link: "/",
   },
   {
     title: "App Store | Social Media",
     content: "Creative Web Design",
-    image: "/assets/images/portfolio-4.jpg",
+    image: portfolio4,
     link: "/",
   },
   {
     title: "App Store | Social Media",
     content: "Creative Web Design",
-    image: "/assets/images/portfolio-5.jpg",
+    image: portfolio5,
     link: "/",
   },
   {
     title: "App Store | Social Media",
     content: "Creative Web Design",
-    image: "/assets/images/portfolio-6.jpg",
+    image: portfolio6,
     link: "/",
   },
 ];
