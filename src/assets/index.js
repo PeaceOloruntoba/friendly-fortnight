fd86ddfd1d5c6b2bdf6a12cec09f8364ca6683c4
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 06:57:13 +0100

    kjh

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,19 @@ import homebottom from "./images/homebottom.png";
 import choose from "./images/choose-1.png";
 import mee from "./images/mee.jpg";
 import sky from "./images/skys.jpg";
+import feature1 from "./images/feature-icon-1.svg";
+import feature2 from "./images/feature-icon-2.svg";
+import feature3 from "./images/feature-icon-3.svg";
 
-export default { logo, aboutimg, jumbotron, homebottom, choose, mee, sky };
+export default {
+  logo,
+  aboutimg,
+  jumbotron,
+  homebottom,
+  choose,
+  mee,
+  sky,
+  feature1,
+  feature2,
+  feature3,
+};
---
+++ src/components/ui/About/Hero.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { feature1, feature2, feature3 } from "@/assets";
+
+export default function Hero() {
+  return (
+    <div>
+      <div className="grid lg:grid-cols-3 grid-cols-1 py-32 px-56 gap-5">
+        <div className="bg-white p-6 text-center rounded-xl shadow-xl shadow-gray-200 group transform transition-transform duration-500 hover:-translate-y-4 flex flex-col items-center justify-center gap-6">
+          <img src={feature1} alt="" className="w-24" />
+          <p className="text-2xl font-semibold">Flexibility &amp; Responsive</p>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad
+            minim veniam
+          </p>
+          <Link
+            to=""
+            className="flex items-center justify-center gap-3 hover:gap-1 duration-500 text-purple-800 font-semibold"
+          >
+            <FaArrowRight />
+            Learn More
+          </Link>
+        </div>
+        <div className="bg-white p-6 text-center rounded-xl shadow-xl shadow-gray-200 group transform transition-transform duration-500 hover:-translate-y-4 flex flex-col items-center justify-center gap-6">
+          <img src={feature2} alt="" className="w-24" />
+          <p className="text-2xl font-semibold">Dedicated Teams</p>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad
+            minim veniam
+          </p>
+          <Link
+            to=""
+            className="flex items-center justify-center gap-3 hover:gap-1 duration-500 text-purple-800 font-semibold"
+          >
+            <FaArrowRight />
+            Learn More
+          </Link>
+        </div>
+        <div className="bg-white p-6 text-center rounded-xl shadow-xl shadow-gray-200 group transform transition-transform duration-500 hover:-translate-y-4 flex flex-col items-center justify-center gap-6">
+          <img src={feature3} alt="" className="w-24" />
+          <p className="text-2xl font-semibold">Focusing on Business</p>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad
+            minim veniam
+          </p>
+          <Link
+            to=""
+            className="flex items-center justify-center gap-3 hover:gap-1 duration-500 text-purple-800 font-semibold"
+          >
+            <FaArrowRight />
+            Learn More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
