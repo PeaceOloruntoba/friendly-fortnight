2cf9c3d85cc047196b37fdeb14e4f0ac39ef968e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-10 12:14:31 +0100

    Update ContactForm.jsx

--- src/components/ui/contact/ContactForm.jsx
+++ src/components/ui/contact/ContactForm.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function ContactForm() {
   return (
-    <div>
-      
+    <div className="flex">
+      <span></span>
     </div>
-  )
+  );
 }
