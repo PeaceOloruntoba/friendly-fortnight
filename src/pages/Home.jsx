3cc24a31f2b392b55e68d76e676ed116a2588126
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-09-25 11:34:41 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../components/ui/Home/Hero";
 import About from "../components/ui/Home/About";
 import Services from "../components/ui/Home/Services";
@@ -6,8 +6,19 @@ import WhyWhy from "../components/ui/Home/WhyWhy";
 import Team from "../components/ui/Home/Team";
 import Contact from "../components/ui/Home/Contact";
 import Testimonial from "../components/ui/Home/Testimonial";
+import { useLocation } from "react-router-dom";
 
 export default function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.replace("#", ""));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
   return (
     <div className="bg-white">
       <Hero />
